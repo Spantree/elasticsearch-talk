eb296493b53123cef42da8f18ae6db31cae974ee
Cedric G Hurst II <cedric@spantree.net>
2013-01-22 17:24:46 -0600

    adding bulk loading

--- presentation/data/DataSplitter.groovy
+++ presentation/data/DataSplitter.groovy
@@ -4,7 +4,7 @@ import groovy.json.JsonSlurper
 import groovy.json.JsonBuilder
 
 // remove any old files starting with `location_`
-new File('.').eachFileMatch( ~/location.*\.json/) { oldFile ->
+new File('.').eachFileMatch( ~/(location.*|bulk)\.json/) { oldFile ->
 	oldFile.delete()
 }
 
@@ -14,13 +14,21 @@ def jsonStr = new File("wikipedia_locations.json").text
 def slurper = new JsonSlurper()
 def json = slurper.parseText(jsonStr)
 
+def bulkFile = new File('bulk.json')
+
 // for each location under the `results` property
 json.results.each { location ->
 	// calculate a slug, replacing whitespace and punctuation with underscores
 	String slug = location.wikipedia_id.replaceAll(/\W+/, '_').toLowerCase()
 	// construct a filename using the slug
 	String filename = "location_${slug}.json"
+	location.id = slug
 	// write the location json file to the calculated filename
-	def out = new JsonBuilder(location)
-	new File(filename) << out.toPrettyString()
+	def docBuilder = new JsonBuilder(location)
+	new File(filename) << docBuilder.toPrettyString()
+	
+	def commandBuilder = new JsonBuilder([index: [_id: slug]])
+	
+	bulkFile << commandBuilder.toString() << '\n'
+	bulkFile << docBuilder.toString() << '\n'
 }

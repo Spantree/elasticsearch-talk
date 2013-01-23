f63a375abc3798b5b4924b4f31f78a91589dbb35
Cedric G Hurst II <cedric@spantree.net>
2013-01-23 14:25:05 -0600

    adding groovy runtime to vagrant install

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,21 +1,19 @@
 node default {
-	stage { 'first': 
-      before => Stage['main'],
-    }
+	# Structure
+	stage { 'first': before => Stage['main'] }
     stage { 'last': }
     Stage['main'] -> Stage['last']
 
-	class { "aptupdate":
-		stage => "first",
-	}
-
+    # Linuxy Stuff
+	class { "aptupdate": stage => "first" }
 	class { "curl": stage => "first" }
-	
-	# Virtual Machines
-	class { "java": }
 
-	# Middleware
-	class { "elasticsearch": 
-		version => "0.20.2"
-	}
+	# Virtual Machines
+	class { "java": stage => "first" }
+
+	# Runtimes
+	class { "groovy": }
+
+	# Hotness
+	class { "elasticsearch": version => "0.20.2" }
 }
---
+++ puppet/modules/groovy/manifests/init.pp
@@ -0,0 +1,5 @@
+class groovy {
+        package { "groovy":
+                ensure => latest;
+        }
+}

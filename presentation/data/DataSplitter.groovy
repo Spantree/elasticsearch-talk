2e2f7c247f6b6ff14d68178d90f53502a71f1c1e
Cedric G Hurst II <cedric@spantree.net>
2013-01-22 21:05:28 -0600

    adding fake last updated times

--- presentation/data/DataSplitter.groovy
+++ presentation/data/DataSplitter.groovy
@@ -16,6 +16,9 @@ def json = slurper.parseText(jsonStr)
 
 def bulkFile = new File('bulk.json')
 
+Date now = new Date()
+Random r = new Random()
+Integer secondsInYear = 365L * 24 * 60 * 60
 // for each location under the `results` property
 json.results.each { location ->
 	// calculate a slug, replacing whitespace and punctuation with underscores
@@ -23,6 +26,8 @@ json.results.each { location ->
 	// construct a filename using the slug
 	String filename = "location_${slug}.json"
 	location._id = slug
+	// pick a random last updated time between now and a year ago
+	location.lastUpdated = new Date(now.time - new Long(r.nextInt(secondsInYear)) * 1000L)
 	// write the location json file to the calculated filename
 	def docBuilder = new JsonBuilder(location)
 	new File(filename) << docBuilder.toPrettyString()

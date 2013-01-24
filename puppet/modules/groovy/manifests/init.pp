20153628aa3e4ee1706e7e51e5f5607e2beb842f
Cedric G Hurst II <cedric@spantree.net>
2013-01-24 11:15:22 -0600

    adding groovy grape dependencies to puppet

--- puppet/modules/groovy/manifests/init.pp
+++ puppet/modules/groovy/manifests/init.pp
@@ -2,4 +2,12 @@ class groovy {
 	package { "groovy":
 		ensure => latest;
 	}
+
+	file { "/home/vagrant/.groovy":
+		ensure => directory,
+		source => "puppet:///modules/groovy/.groovy",
+		owner => vagrant,
+		group => vagrant,
+		recurse => true
+	}
 }

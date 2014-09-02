816cdc0ad84524bed3ac3f8f5e02bb821e8584ce
Cedric Hurst <cedric@spantree.net>
2014-09-02 14:51:47 -0500

    updating packer approach

--- packer/zerodisk.sh
+++ packer/zerodisk.sh
@@ -1,11 +1,3 @@
-echo "Performing one last sitewide upgrade"
-apt-get update 
-apt-get -y dist-upgrade
-
-echo "Cleaning home directory for vagrant user"
-cd $HOME
-rm -rf *.*
-
 echo "Zeroing the free space to save space in the final image"
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY

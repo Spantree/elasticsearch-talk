3a02c496a3675432b7ef18fbc471970a821cb884
Cedric Hurst <cedric@spantree.net>
2014-09-15 10:53:47 -0500

    sense editor: adding ability to specify section number

--- puppet/modules/elasticsearch_talk/files/sense/app/spantree.senseEditorReplace.js
+++ puppet/modules/elasticsearch_talk/files/sense/app/spantree.senseEditorReplace.js
@@ -1,5 +1,9 @@
 // make sure all of ace is loaded first.
 require(['require','ace'], function (require) { require(['app'], function() {
+  RegExp.quote = function(str) {
+      return (str+'').replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+  };
+
   var getEditor = function() {
     var ace = require('ace');
     return ace.edit('editor');
@@ -17,8 +21,20 @@ require(['require','ace'], function (require) { require(['app'], function() {
   var gotoLineNumber = function() {
     var editor = getEditor();
     var lineNumber = window.location.hash.split(',L')[1];
+    var sectionNumber = window.location.hash.split(',S')[1];
+    if(sectionNumber) {
+      var regex = new RegExp("^[#\\s]+" + RegExp.quote(sectionNumber + ":"));
+      var lines = editor.getSession().getDocument().$lines;
+      for(var i = 0; i<lines.length; i++) {
+        var line = lines[i];
+        if(regex.test(line)) {
+          lineNumber = i+1;
+          break;
+        }
+      }
+    }
     if(lineNumber) {
-      var lineInt = parseInt(lineNumber)
+      var lineInt = parseInt(lineNumber);
       editor.gotoLine(lineInt);
       editor.scrollToLine(Math.max(lineInt-1, 1), false, true);
     }
@@ -30,7 +46,7 @@ require(['require','ace'], function (require) { require(['app'], function() {
     // read the hash of the url
     var hash = window.location.hash;
     if(hash) {
-      var senseName = hash.replace(/^#/, '').split(',L')[0];
+      var senseName = hash.replace(/^#/, '').split(',L')[0].split(',S')[0];
       // if the hash exists, retrieve the corresponding `.sense` file
       // from the nginx server running on port 80
       var url = "http://" + window.location.hostname + "/" + senseName + ".sense";
